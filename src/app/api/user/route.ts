5fa6423991438e9536bc8e503f26e67ddbcfb10b
Tuuduu <munhtogtohganbat@yahoo.com>
2024-03-31 12:21:54 +0800

    first comment

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import db from "@/lib/mysql";
+
+export async function GET() {
+  try {
+    const results = await new Promise((resolve, reject) => {
+      db.query("SELECT * FROM user", (err: any, results: []) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+    console.log(results);
+    return NextResponse.json(results);
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
