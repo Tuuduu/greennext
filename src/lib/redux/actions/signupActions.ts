fecf50cabeddd96fbc171036892c6e9deb086291
Tuuduu <munhtogtohganbat@yahoo.com>
2024-04-03 19:17:57 +0800

    add session

---
+++ src/lib/redux/actions/signupActions.ts
@@ -0,0 +1,21 @@
+export const signupUser = () => {
+  return {
+    type: "",
+  };
+};
+export const signupUserStart = () => {
+  return {
+    type: "",
+  };
+};
+
+export const signupUserSuccess = () => {
+  return {
+    type: "",
+  };
+};
+export const signupUserError = () => {
+  return {
+    type: "",
+  };
+};
