87eba7513e76814c7f648c9db05e5540a1837137
Tuuduu <tuuduuganbat@gmail.com>
2025-01-07 23:17:09 +0800

    ticketTable засаж болгов

--- src/components/dashboard/ticket-order/TicketTable.tsx
+++ src/components/dashboard/ticket-order/TicketTable.tsx
@@ -1,16 +1,49 @@
-import { fetchMtaTicket } from '@/library/mongoDB/data';
-import TicketList from './TicketList';
+"use client";
+import { fetchMtaTicket } from "@/library/mongoDB/data";
+import TicketList from "./TicketList";
+import { useState, useEffect } from "react";
 
-const TicketTable = async () => {
+const TicketTable = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const ticketData = await fetchMtaTicket();
-    const serializedTickets = JSON.parse(JSON.stringify(ticketData));
+  useEffect(() => {
+    // API дуудлага хийх
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/mta-ticket-order", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ someData: "example" }),
+        });
 
-    return (
-        <div className='w-full h-full bg-gray-50'>
-            <TicketList ticket={serializedTickets}/>
-        </div>
-    );
-}
+        const result = await response.json();
+        console.log("dataaa --->", result);
+        setData(result);
+      } catch (error) {
+        console.error("API fetch error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); // хоосон dependency array: зөвхөн нэг удаа ажиллана
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!data) {
+    return <p>No data available</p>;
+  }
+  return (
+    <div className="w-full h-full bg-gray-50">
+      <TicketList ticket={data} />
+    </div>
+  );
+};
 
 export default TicketTable;
