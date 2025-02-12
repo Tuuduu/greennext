0f849ad8ef2cbfb0df3323199375692420b27605
Tuuduu <munhtogtohganbat@yahoo.com>
2025-02-12 10:25:02 +0800

    connect.ts fix

--- src/library/mongoDB/connect.ts
+++ src/library/mongoDB/connect.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
   try {
-    await mongoose.connect("mongodb://172.19.10.27/greennext");
+    await mongoose.connect("mongodb://localhost/greennext");
     console.log("Connected to MongoDB server");
   } catch (error) {
     console.log("Error while connecting. !!!", error);
